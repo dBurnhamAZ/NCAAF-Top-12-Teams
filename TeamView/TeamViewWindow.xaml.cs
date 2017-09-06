876d7498cd640c13b44bd3b5269ce23ad8f20933
Dennis Burnham <dennisbaz@gmail.com>
2017-09-05 20:30:16 -0700

    TeamView.UI
    Main XAML Window

---
+++ TeamView/TeamViewWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using NCAAFTeamViewer.Presentation;
+using System.Windows;
+
+namespace TeamView.View
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class TeamViewWindow : Window
+    {
+        public TeamViewWindow(TeamViewViewModel viewModel)
+        {
+            InitializeComponent();
+            DataContext = viewModel;
+        }
+    }
+}
