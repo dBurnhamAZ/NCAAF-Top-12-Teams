37d02153aee1fa48b074d03a66845292d0a3baa8
Dennis Burnham <dennisbaz@gmail.com>
2017-09-05 20:13:40 -0700

    Application.Unity
    Separate app with Unity IoC Containers for Dependency Injection

---
+++ TeamView.Unity/App.xaml.cs
@@ -0,0 +1,40 @@
+﻿using NCAAFRepository.CSV;
+using NCAAFTeamViewer.Presentation;
+using System.Windows;
+using Microsoft.Practices.Unity;
+using NCAAFRepository.Interface;
+using NCAAFRepository.Service;
+
+namespace TeamView.View
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        IUnityContainer container;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            ConfigureContainer();
+            ComposeObjects();            
+            Application.Current.MainWindow.Show();
+        }
+
+        private void ConfigureContainer()
+        {
+            container = new UnityContainer();
+            //default Transiet Registered Type
+            container.RegisterType<ITeamRepository, CSVRepository>(default(TransientLifetimeManager));
+            //set Singleton Registered Type
+            //container.RegisterType<ITeamRepository, CSVRepository>(new ContainerControlledLifetimeManager());
+        }
+
+        private void ComposeObjects()
+        {
+            Application.Current.MainWindow = container.Resolve<TeamViewWindow>();
+            Application.Current.MainWindow.Title = "Loose Coupling - Team Viewer";
+        }
+    }
+}
