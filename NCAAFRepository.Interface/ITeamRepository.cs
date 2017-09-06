9a591767d4ed209e3d905cf44e397b1a08060624
Dennis Burnham <dennisbaz@gmail.com>
2017-09-05 20:20:34 -0700

    NCAAFRepository.Interface
    Interface for decoupling Repositories to ViewModel

---
+++ NCAAFRepository.Interface/ITeamRepository.cs
@@ -0,0 +1,20 @@
+﻿using NCAAFRankingViewer.SharedObjects;
+using System.Collections.Generic;
+
+namespace NCAAFRepository.Interface
+{
+    public interface ITeamRepository
+    {                
+        IEnumerable<CollegeTeams> GetColleges();
+        
+        CollegeTeams GetCollege(string teamName);
+        
+        void AddCollege(CollegeTeams newCollege);
+        
+        void UpdateCollege(string planetName, CollegeTeams updatedCollege);
+        
+        void DeleteCollege(string teamName);
+        
+        void UpdateColleges(List<CollegeTeams> updatedColleges);
+    }
+}
