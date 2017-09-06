90469d0c2e81200e8357073cd322edd956041ccf
Dennis Burnham <dennisbaz@gmail.com>
2017-09-05 20:16:06 -0700

    NCAAFRankingViewer.SharedObjects
    Shared class for entire application

---
+++ NCAAFRankingViewer.SharedObjects/CollegeTeams.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Media.Imaging;
+
+namespace NCAAFRankingViewer.SharedObjects
+{
+    public class CollegeTeams
+    {
+        public string CollegeTeamName { get; set; }
+
+        public string CollegeLocation { get; set; }
+
+        public int CollegeRanking { get; set; }
+
+        public Uri CollegeIcon { get; set; }
+
+        public DateTime DateOfRanking { get; set; }
+    }
+}
