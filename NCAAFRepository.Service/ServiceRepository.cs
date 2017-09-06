1f23fd6e14634f29b05fc0d387335435288171f5
Dennis Burnham <dennisbaz@gmail.com>
2017-09-05 20:23:30 -0700

    NCAAFRepository.Service
    Service repository for WCF Services communications

---
+++ NCAAFRepository.Service/ServiceRepository.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using NCAAFRepository.Interface;
+using System.Linq;
+using NCAAFRankingViewer.SharedObjects;
+using NCAAFRepository.Service.ServiceReference1;
+
+namespace NCAAFRepository.Service
+{
+    public class ServiceRepository : ITeamRepository
+    {
+        private INCAAFLatestRankings _serviceProxy;
+        public INCAAFLatestRankings ServiceProxy
+        {
+            get
+            {
+                if (_serviceProxy == null)
+                    _serviceProxy = new NCAAFLatestRankingsClient();
+                    return _serviceProxy;
+            }
+            set
+            {
+                if (_serviceProxy == value)
+                    return;
+                _serviceProxy = value;
+            }
+        }
+
+        public void AddCollege(CollegeTeams newCollege)
+        {
+            ServiceProxy.AddCollege(newCollege);
+        }
+
+        public void DeleteCollege(string teamName)
+        {
+            ServiceProxy.DeleteCollege(teamName);
+        }
+
+        public CollegeTeams GetCollege(string teamName)
+        {
+            return ServiceProxy.GetCollege(teamName);
+        }
+
+        public IEnumerable<CollegeTeams> GetColleges()
+        {
+            return ServiceProxy.GetColleges();
+        }
+
+        public void UpdateCollege(string teamName, CollegeTeams updatedCollege)
+        {
+            ServiceProxy.UpdateCollege(teamName, updatedCollege);
+        }
+
+        public void UpdateColleges(List<CollegeTeams> updatedColleges)
+        {
+            ServiceProxy.UpdateColleges(updatedColleges.ToList());
+        }
+        
+    }
+}
