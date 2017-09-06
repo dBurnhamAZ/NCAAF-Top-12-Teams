5855eb1813a72e5329bab1872a0d1c87abcdb667
Dennis Burnham <dennisbaz@gmail.com>
2017-09-05 20:08:55 -0700

    Application.Wrapper
    Separate application that uses Dependency Injection with manual IoC

---
+++ TeamView.View/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using NCAAFRepository.CSV;
+using NCAAFRepository.Service;
+using NCAAFTeamViewer.Presentation;
+using System.Windows;
+
+namespace TeamView.View
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            ComposeObjects();
+            Application.Current.MainWindow.Title = "Loose Coupling - Team Viewer";
+            Application.Current.MainWindow.Show();
+        }
+
+        private void ComposeObjects()
+        {
+            var repository = new CSVRepository();
+            var viewModel = new TeamViewViewModel(repository);
+            Application.Current.MainWindow = new TeamViewWindow(viewModel);
+        }
+    }
+}
---
+++ TeamView.View/ColorConverter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace TeamView.View
+{
+    public class ColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string team = value.ToString();
+
+            switch (team)
+            {
+                case "Alabama":
+                    return new SolidColorBrush(Colors.Red);
+                case "Ohio State":
+                    return new SolidColorBrush(Colors.Pink);
+                case "Florida State":
+                    return new SolidColorBrush(Colors.Maroon);
+                case "USC":
+                    return new SolidColorBrush(Colors.Gold);
+                case "Clemson":
+                    return new SolidColorBrush(Colors.Orange);
+                case "Penn State":
+                    return new SolidColorBrush(Colors.Blue);
+                case "Oklahoma":
+                    return new SolidColorBrush(Colors.Red);
+                case "Washington":
+                    return new SolidColorBrush(Colors.Blue);
+                case "Winconsin":
+                    return new SolidColorBrush(Colors.Red);
+                case "Oklahoma State":
+                    return new SolidColorBrush(Colors.Orange);
+                case "MICHIGAN":
+                    return new SolidColorBrush(Colors.DarkBlue);
+                case "Auburn":
+                    return new SolidColorBrush(Colors.Maroon);
+                default:
+                    return new SolidColorBrush(Colors.Blue);
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
