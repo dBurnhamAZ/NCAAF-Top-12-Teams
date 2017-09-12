62007d35a76de3ffea8b19e72e7b8c698590ff74
Dennis Burnham <dennisbaz@gmail.com>
2017-09-11 23:14:55 -0700

    Add files via upload

---
+++ IoCCustomContainer/Services/TransientCreationService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoCCustomContainer.Services
+{
+    internal class TransientCreationService
+    {
+        static TransientCreationService instance = null;
+
+        static TransientCreationService()
+        {
+            instance = new TransientCreationService();
+        }
+
+        private TransientCreationService()
+        { }
+
+        public static TransientCreationService GetInstance()
+        {
+            return instance;
+        }
+
+        public object GetNewObject(Type t, object[] arguments = null)
+        {
+            object obj = null;
+
+            try
+            {
+                obj = Activator.CreateInstance(t, arguments);
+            }
+            catch
+            {
+                // send it to a logger
+            }
+
+            return obj;
+        }
+    }
+}
