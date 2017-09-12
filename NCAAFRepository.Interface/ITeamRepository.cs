0d77b248a82f148404035c8c906d230657fcf286
Dennis Burnham <dennisbaz@gmail.com>
2017-09-11 23:15:38 -0700

    Add files via upload

--- NCAAFRepository.Interface/ITeamRepository.cs
+++ NCAAFRepository.Interface/ITeamRepository.cs
@@ -4,14 +4,14 @@ using System.Collections.Generic;
 namespace NCAAFRepository.Interface
 {
     public interface ITeamRepository
-    {                
+    {       
         IEnumerable<CollegeTeams> GetColleges();
         
         CollegeTeams GetCollege(string teamName);
         
         void AddCollege(CollegeTeams newCollege);
         
-        void UpdateCollege(string planetName, CollegeTeams updatedCollege);
+        void UpdateCollege(string teamName, CollegeTeams updatedCollege);
         
         void DeleteCollege(string teamName);
         
