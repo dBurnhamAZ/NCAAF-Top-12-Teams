a9ec280e6636c546e3b60f276579d91acac3a7c8
Dennis Burnham <dennisbaz@gmail.com>
2017-09-11 23:07:38 -0700

    Add files via upload

--- TeamView.View/App.xaml.cs
+++ TeamView.View/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using NCAAFRepository.CSV;
+using NCAAFRepository.GetLatestRankings;
 using NCAAFRepository.Service;
 using NCAAFTeamViewer.Presentation;
 using System.Windows;
@@ -18,10 +19,37 @@ namespace TeamView.View
             Application.Current.MainWindow.Show();
         }
 
+        /// <summary>
+        /// App.xaml.cs will control the OnStart injection.
+        /// In this example, all we have to do is unComment the Repository class
+        /// for any desired start-up.
+        /// This example has options of running a Service (WCF), a CSV file, or
+        /// updated values directly from a NCAA Standings Webpage. 
+        /// The purpose of this app is to demo decoupling and Depenency Injection
+        /// By seperating each layer, we can use Interfaces for communications
+        /// and Unit Testing
+        /// </summary>
         private void ComposeObjects()
         {
-            var repository = new CSVRepository();
+            var repository = new PullLatestRankingsFromWebRepository();            
+            //var repository = new CSVRepository();
+           // var repository = new ServiceRepository();
+
             var viewModel = new TeamViewViewModel(repository);
+
+            if (repository.GetType().Name == "CSVRepository")
+            {
+                viewModel.Repository = "CSVRepository";
+            }
+            else if(repository.GetType().Name == "ServiceRepository")
+            {
+                viewModel.Repository = "ServiceRepository";
+            }
+            else if(repository.GetType().Name == "PullLatestRankingsFromWebRepository")
+            {
+                viewModel.Repository = "WebsiteRepository";
+            }
+            
             Application.Current.MainWindow = new TeamViewWindow(viewModel);
         }
     }
